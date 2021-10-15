614ea38c2db77731fab28ed65daa6315a7ee67e5
Aditya Raj <rajaditya079@gmail.com>
2021-10-15 22:11:12 +0530

    Logos added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'dart:ui';
-
-import 'package:fitegrate_project/pallete.dart';
+import 'dart:core';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fitegrate_project/pallete.dart';
 import 'rounded_button.dart';
-
-
-
 
 void main() {
   runApp(MyApp());
@@ -18,13 +16,35 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.light(),
+      darkTheme:
+          ThemeData.dark(), // standard dark theme // device controls theme
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
+}
+
+Widget _buildSocialBtn(Function onTap, AssetImage logo) {
+  return GestureDetector(
+    child: Container(
+      height: 40.0,
+      width: 40.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, 2),
+            blurRadius: 4.0,
+          ),
+        ],
+        image: DecorationImage(
+          image: logo,
+        ),
+      ),
+    ),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
@@ -36,245 +56,238 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    Size size  = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
-      body:  SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(16, 35, 16, 8),
           child: Column(
-              
-               mainAxisAlignment: MainAxisAlignment.start,
-               //crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Image(image: AssetImage('assets/black_logo.jpeg'), height: 166, width: 147),
-                SizedBox(height: size.height*0.02,),
-      
-                Container(
-                  height:size.height * 0.08,
-                  width: size.width * 0.8,
-                  decoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Center(
-            child: TextField(
-              onChanged: (value)
-              {
-                setState(() {
-                  //TextInputFieldEmail.email=value;
-                });
-              },
-      
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  // child: Icon(Icons.email, size: 28, ),
-                ),
-                hintText: 'Name',
-                hintStyle: TextStyle(color: Colors.black87, fontSize: 18, fontWeight: FontWeight.bold),
+            mainAxisAlignment: MainAxisAlignment.start,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                  image: AssetImage('assets/black_logo.jpeg'),
+                  height: 166,
+                  width: 147),
+              SizedBox(
+                height: size.height * 0.02,
               ),
-              
-              keyboardType: TextInputType.name,
-              textInputAction: TextInputAction.next,
-            ),
-          ),
-        
-                 ),
-          //        Container(
-          // height:size.height * 0.08,
-          // width: size.width * 0.8,
-          // decoration: BoxDecoration(
-          //   color: Colors.orange,
-          //   borderRadius: BorderRadius.circular(16),
-          // ),
-          //        ),
-      
-                 SizedBox(height: size.height*0.02,),
-                 Container(
-                  height:size.height * 0.08,
-                  width: size.width * 0.8,
-                  decoration: BoxDecoration(
-                  color: Colors.orange,
+
+              Container(
+                height: size.height * 0.08,
+                width: size.width * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.orange[300],
                   borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Center(
-            child: TextField(
-              onChanged: (value)
-              {
-                setState(() {
-                  //TextInputFieldEmail.email=value;
-                });
-              },
-      
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  //child: Icon(Icons.email, size: 28, ),
                 ),
-                hintText: 'Email id',
-                hintStyle: TextStyle(color: Colors.black87, fontSize: 18, fontWeight: FontWeight.bold),
+                child: Center(
+                  child: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        //TextInputFieldEmail.email=value;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        // child: Icon(Icons.email, size: 28, ),
+                      ),
+                      hintText: 'Name',
+                      hintStyle: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    keyboardType: TextInputType.name,
+                    textInputAction: TextInputAction.next,
+                  ),
+                ),
               ),
-              
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.next,
-            ),
-          ),
-        
-                 ),
-      
-                  SizedBox(height: size.height*0.02,),
-                 Container(
-                  height:size.height * 0.08,
-                  width: size.width * 0.8,
-                  decoration: BoxDecoration(
-                  color: Colors.orange,
+              //        Container(
+              // height:size.height * 0.08,
+              // width: size.width * 0.8,
+              // decoration: BoxDecoration(
+              //   color: Colors.orange,
+              //   borderRadius: BorderRadius.circular(16),
+              // ),
+              //        ),
+
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Container(
+                height: size.height * 0.08,
+                width: size.width * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.orange[300],
                   borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Center(
-            child: TextField(
-              onChanged: (value)
-              {
-                setState(() {
-                  //TextInputFieldEmail.email=value;
-                });
-              },
-      
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  //child: Icon(Icons.email, size: 28, ),
                 ),
-                hintText: 'Password',
-                hintStyle: TextStyle(color: Colors.black87, fontSize: 18, fontWeight: FontWeight.bold),
+                child: Center(
+                  child: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        //TextInputFieldEmail.email=value;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        //child: Icon(Icons.email, size: 28, ),
+                      ),
+                      hintText: 'Email id',
+                      hintStyle: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                  ),
+                ),
               ),
-              
-              keyboardType: TextInputType.emailAddress,
-              obscureText: true,
-              textInputAction: TextInputAction.next,
-            ),
-          ),
-        
-                 ),
-      
-                  SizedBox(height: size.height*0.02,),
-                 Container(
-                  height:size.height * 0.08,
-                  width: size.width * 0.8,
-                  decoration: BoxDecoration(
-                  color: Colors.orange,
+
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Container(
+                height: size.height * 0.08,
+                width: size.width * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.orange[300],
                   borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Center(
-            child: TextField(
-              onChanged: (value)
-              {
-                setState(() {
-                  //TextInputFieldEmail.email=value;
-                });
-              },
-      
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  //child: Icon(Icons.email, size: 28, ),
                 ),
-                hintText: 'Confirm Password',
-                hintStyle: TextStyle(color: Colors.black87, fontSize: 18, fontWeight: FontWeight.bold),
+                child: Center(
+                  child: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        //TextInputFieldEmail.email=value;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        //child: Icon(Icons.email, size: 28, ),
+                      ),
+                      hintText: 'Password',
+                      hintStyle: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: true,
+                    textInputAction: TextInputAction.next,
+                  ),
+                ),
               ),
-              
-              keyboardType: TextInputType.emailAddress,
-              obscureText: true,
-              textInputAction: TextInputAction.next,
-            ),
+
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Container(
+                height: size.height * 0.08,
+                width: size.width * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.orange[300],
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Center(
+                  child: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        //TextInputFieldEmail.email=value;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        //child: Icon(Icons.email, size: 28, ),
+                      ),
+                      hintText: 'Confirm Password',
+                      hintStyle: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: true,
+                    textInputAction: TextInputAction.next,
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+
+              RoundedButton(buttonName: 'Sign Up'),
+
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+
+              Text(
+                'Or Sign Up with',
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _buildSocialBtn(
+                    () => print('Login with Facebook'),
+                    AssetImage(
+                      'assets/logos/facebook.jpg',
+                    ),
+                  ),
+                  _buildSocialBtn(
+                    () => print('Login with Google'),
+                    AssetImage(
+                      'assets/logos/google.jpg',
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account? ',
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Sign in',
+                    style: TextStyle(
+                        color: Colors.blue[800],
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ],
           ),
-        
-                 ),
-      
-                 SizedBox(height: size.height*0.02,),
-      
-                 RoundedButton(buttonName: 'Sign Up'),
-      
-                 SizedBox(height: size.height*0.04,),
-      
-                 Text('Or sign up with', style: TextStyle(color: Colors.black87, fontSize: 20, fontWeight: FontWeight.bold),),
-      
-        SizedBox(height: size.height*0.02,),
-            
-              
-              
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          InkWell(
-        onTap: () {},
-        child: Ink(
-          color: Color(0xFF397AF3),
-          child: Padding(
-        padding: EdgeInsets.all(6),
-        child: Wrap(
-          crossAxisAlignment: WrapCrossAlignment.center,
-          children: [
-            Icon(Icons.android), 
-            SizedBox(width: 12),
-            Text('Google'),
-          ],
-        ),
-          ),
         ),
       ),
-      
-      SizedBox( width: size.width * 0.08,),
-      
-      InkWell(
-        onTap: () {},
-        child: Ink(
-          color: Color(0xFF397AF3),
-          child: Padding(
-        padding: EdgeInsets.all(6),
-        child: Wrap(
-          crossAxisAlignment: WrapCrossAlignment.center,
-          children: [
-            Icon(Icons.android), 
-            SizedBox(width: 12),
-            Text('Facebook'),
-          ],
-        ),
-          ),
-        ),
-      ),
-      
-          
-        ],
-      ),
-      
-      SizedBox(height: size.height*0.04,),
-      
-      Row(
-         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Already have an account? ', style: TextStyle(color: Colors.black87, fontSize: 20, fontWeight: FontWeight.bold),),
-          Text('Sign in', style: TextStyle(color: Colors.blue[800], fontSize: 20, fontWeight: FontWeight.bold),)
-      
-        ],
-      ),
-      
-      
-      
-      
-            
-              ],
-            ),
-        ),
-      ),
-      
     );
   }
 }
